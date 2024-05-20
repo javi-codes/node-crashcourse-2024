8b0b151ecbc5eb3982706091ebc5dd6f93bd4d22
Javier Ramirez Roa <j.ramirez.roa64@gmail.com>
2024-05-20 15:48:27 -0700

    Moved routes into its own directory and file.

--- server.js
+++ server.js
@@ -1,39 +1,18 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import path from "path";
+import posts from "./routes/posts.js";
 const port = process.env.PORT || 8000;
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // Setting up static folder
 // app.use(express.static(path.join(__dirname, "public")));
-let posts = [
-  { id: 1, title: "Post 1" },
-  { id: 2, title: "Post 2" },
-  { id: 3, title: "Post 3" },
-  { id: 4, title: "Post 4" },
-];
 
-// Get all posts
-app.get("/api/posts", (req, res) => {
-  const limit = parseInt(req.query.limit);
-
-  if (!isNaN(limit) && limit > 0) {
-    return res.status(200).json(posts.slice(0, limit));
-  }
-  res.status(200).json(posts);
-});
-
-// Get a single post
-app.get("/api/posts/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = posts.find((post) => post.id === id);
-
-  if (!post) {
-    return res
-      .status(404)
-      .json({ msg: `A post with the id of ${id} was not found` });
-  }
-  res.status(200).json(post);
-});
+// Routes
+app.use("/api/posts", posts);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
