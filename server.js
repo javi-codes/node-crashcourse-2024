2076f5d76d2e3a10df629ad0f721a3edb1f19bb7
Javier Ramirez Roa <j.ramirez.roa64@gmail.com>
2024-05-20 00:10:40 -0700

    WIP: Created static array of "posts", have two basic get routes setup.

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 8000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello world!</h1>");
+// Setting up static folder
+// app.use(express.static(path.join(__dirname, "public")));
+let posts = [
+  { id: 1, title: "Post 1" },
+  { id: 2, title: "Post 2" },
+  { id: 3, title: "Post 3" },
+  { id: 4, title: "Post 4" },
+];
+
+// Get all posts
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
 });
 
-app.listen(8000, () => console.log(`Server is running on port 8000`));
+// Get a single post
+app.get("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const post = posts.filter((post) => post.id === id);
+
+  res.json(post);
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
